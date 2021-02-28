a1ebdfd9f68ef9192f805cfaa6fa274f135f5d70
Trinh Tran <kieutrinh.ibc@gmail.com>
2021-02-28 17:44:35 +0200

    README.md is added

---
+++ README.md
@@ -0,0 +1,17 @@
+## Table of contents
+* [General info](#general-info)
+* [Work flow](#work flow)
+
+## General info
+This is a Python program that read lists of Http URLs and corresponding requirements from a configuration file(websiteList.txt) then verify if it fulfills a defined requirement. Progress of the request as well as its reponse time is displayed into a log file name log_file.txt
+	
+## Work flow
+Program follows synchronous work flow as following:
+* Loop through websiteList.txt and extract url addresses and its requirement
+* A http request is sent to url address which was extracted from the previous step
+* Write to log_file.txt a line including url address and it's status which is now "Waiting" 
+* Extract page content, response time, response status code from request's response
+* Check if server is up or down base on response status code. Return "Client Error" ( if response status code is from 400-499). Return "Server is down!" (if response status is from 400-499). Otherwise, checking if the response contains requirement string or not. If yes return "OK", if not return "The content requirements were not fulfilled"
+* Remove the last line of log_file.txt and add a new line which contains the url address, page status and response time to it. 
+* Set timer using time.sleep(sys.argv[1]). The program is automatically run every 5 second (Defaut setting in launch.json file)
+
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import requests
 import sys
 import time
+from typing import Final
+
+#Constants
+URL_FILE: Final = 'websiteList.txt'
+LOG_FILE: Final = 'log_file.txt'
+LOG_FILE: Final = 'log_file.txt'
 
 def update_page_status(new_status, url, request_time):
-    f = open("log_file.txt", "r+")
+    f = open(LOG_FILE, "r+")
     for line in f:
         split_line = line.split("---")
         if line.split("---")[0] == url:
@@ -12,12 +18,12 @@ def update_page_status(new_status, url, request_time):
     return line
 
 def write_log_file(content):
-    log_file = open("log_file.txt","w")
+    log_file = open(LOG_FILE,"w")
     log_file.write(content)
     log_file.close()
 
 def check_page_status():
-    site_list = open("websiteList.txt", "r")
+    site_list = open(URL_FILE, "r")
     output = ""
     for line in site_list:
         url_address = line.split(",")[0]
@@ -37,8 +43,8 @@ def check_page_status():
 
 def initiate_status_to_log_file(line):
     url_address = line.split(",")[0]
-    page_status = "Waiting"
-    loading_text = url_address + "---" + page_status + "---"
+    request_starting_status = "Waiting"
+    loading_text = url_address + "---" + request_starting_status + "---"
     return loading_text
 
 
@@ -51,7 +57,7 @@ def check_status_code(check_result, response_status_code):
         elif response_status_code in range(500, 599):
             page_status = "Server is down!"
         else:
-            page_status = "Page content requirement is not met"
+            page_status = "The content requirements were not fulfilled"
     return page_status
 
 
